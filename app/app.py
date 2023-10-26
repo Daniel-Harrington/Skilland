2c50a4539a93f30a53dde04bfbd933574a4aed29
Daniel Harrington <novus@hotmail.com>
2023-10-26 16:45:45 -0400

    Added grabbing login data
    yup

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -7,9 +7,35 @@ def main():
     return render_template('main.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        # Then get the data from the form
+        username = request.form['username']
+        password = request.form['password']
+        # Get the user associated with these credentials
+        
+
+        return 'The credentials were %s and %s' % (username, password) 
+        # Or you could have a custom template for displaying the info
+        # return render_template('asset_information.html',
+        #                        username=user, 
+        #                        password=password)
+
+    # Otherwise this was a normal GET request
+    else:   
+        return render_template('login.html')
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signupquiz.html')
+
+@app.route("/match")
+def match():
+    return render_template('match.html')
 
 
 
